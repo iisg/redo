8356c62051524a10469ac16ea2b8c63dd6866cbf
Wojciech Frącz <fraczwojciech@gmail.com>
2016-09-27 15:49:54 +0200

    [REPEKA-13] Global project config (NPM)
    Wonder why not Gulp? See:
     * https://goo.gl/KpaXwV
     * https://www.keithcirkel.co.uk/how-to-use-npm-as-a-build-tool/
    
    Change-Id: I457379d67f68b7166eb52f6a553a5fe4513c9ec1

---
+++ scripts/help.js
@@ -0,0 +1,24 @@
+var project = require("./repeka");
+var chalk = require("chalk");
+
+var printHelp = function (help) {
+    project.printAsciiLogoAndVersion();
+
+    console.log(chalk.grey("Usage: npm run ") + chalk.bgYellow(chalk.black("task")));
+    console.log("");
+
+    for (var task in help) {
+        console.log(chalk.yellow(task) + "\t" + help[task]);
+    }
+}
+
+module.exports = {
+    printHelp: printHelp
+};
+
+var runningAsScript = require.main === module;
+if (runningAsScript) {
+    printHelp({
+        "install": "Install project dependencies"
+    });
+}
---
+++ scripts/version.js
@@ -0,0 +1,10 @@
+var describe = require('git-describe');
+
+var version = describe.gitDescribeSync(__dirname);
+var semver = version.semver;
+var appVersion = [semver.major, semver.minor, semver.patch + version.distance].join('.');
+
+module.exports = {
+    text: appVersion,
+    full: describe
+};
