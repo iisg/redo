2a0fcf736e0423a3474771b3107808585f3e6db1
Wojciech Frącz <fraczwojciech@gmail.com>
2016-10-18 10:20:08 +0200

    [REPEKA-10] Switch from phpspec to phpunit completely
    Change-Id: I27c8b482c343da694b7b1844c211dde10a316686

--- src/CoreModule/Domain/Assert/Assertion.php
+++ src/CoreModule/Domain/Assert/Assertion.php
@@ -3,8 +3,6 @@ declare (strict_types = 1);
 namespace Repeka\CoreModule\Domain\Assert;
 
 use Assert\Assertion as BaseAssertion;
-use Repeka\CoreModule\Domain\Exception\AssertionFailedException;
 
 class Assertion extends BaseAssertion {
-    protected static $exceptionClass = AssertionFailedException::class;
 }
--- src/CoreModule/Domain/Exception/AssertionFailedException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare (strict_types = 1);
-namespace Repeka\CoreModule\Domain\Exception;
-
-use Assert\InvalidArgumentException;
-
-class AssertionFailedException extends InvalidArgumentException {
-}
