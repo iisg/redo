22e3f309147c09ca3eb31c55b43911362928f0f6
Wojciech Frącz <fraczwojciech@gmail.com>
2016-10-18 10:20:08 +0200

    [REPEKA-10] Switch from phpspec to phpunit completely
    Change-Id: I27c8b482c343da694b7b1844c211dde10a316686

---
+++ src/CoreModule/Tests/Domain/EmailAddressTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Repeka\CoreModule\Domain;
+
+class EmailAddressTest extends \PHPUnit_Framework_TestCase {
+    public function testCreatingValidEmailAddress() {
+        new EmailAddress('valid@email.pl');
+    }
+
+    /**
+     * @expectedException Assert\InvalidArgumentException
+     */
+    public function testCreatingInvalidEmailAddress() {
+        new EmailAddress('invalid');
+    }
+
+    public function testGettingDomain() {
+        $email = new EmailAddress('valid@email.pl');
+        $this->assertEquals('email.pl', $email->getDomainFromEmailAddress());
+    }
+
+}
