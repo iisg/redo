2dcbf7e8ace2a13ba6279c5a37ced9c279af57f8
Wojciech Frącz <fraczwojciech@gmail.com>
2016-11-28 11:08:39 +0100

    [NT] Simplify add forms
    Change-Id: I0302b6030ad68a98fa0456f452edd7387ebe27d5

--- src/AdminPanel/src/resources-config/language-config/language-list.ts
+++ src/AdminPanel/src/resources-config/language-config/language-list.ts
@@ -1,23 +1,15 @@
 import {HttpClient} from "aurelia-http-client";
 import {autoinject} from "aurelia-dependency-injection";
-import {computedFrom} from "aurelia-binding";
 import {ComponentAttached} from "aurelia-templating";
 import {Language} from "./language";
 import {FloatingAddFormController} from "../add-form/floating-add-form";
 
 @autoinject
 export class LanguageList implements ComponentAttached {
-  newLanguage: Language = new Language;
-  isValidating: boolean = false;
   languageList: Language[];
   addFormController: FloatingAddFormController = {};
 
   constructor(private httpClient: HttpClient) {
-  }
-
-  @computedFrom('isValidating', 'httpClient.isRequesting')
-  get isRequesting() {
-    return this.isValidating || this.httpClient.isRequesting;
   }
 
   attached(): void {
@@ -26,10 +18,9 @@ export class LanguageList implements ComponentAttached {
     });
   }
 
-  addNewLanguage() {
-    return this.httpClient.post('languages', this.newLanguage).then(response => {
+  addNewLanguage(newLanguage: Language) {
+    return this.httpClient.post('languages', newLanguage).then(response => {
       this.addFormController.hide();
-      this.newLanguage = new Language;
       this.languageList.push(response.content);
     });
   }
