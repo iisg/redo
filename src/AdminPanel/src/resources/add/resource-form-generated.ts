71b45b7ea30ee0a0eaf0d3014880cfca6f3c2ea0
Marcin <nikielmcn@gmail.com>
2017-01-11 15:18:53 +0100

    [REPEKA-111] Use multilingual placeholder/label in resource form
    Change-Id: I9c1463385a22891fc9813e83b905218b6d930c50

--- src/AdminPanel/src/resources/add/resource-form-generated.ts
+++ src/AdminPanel/src/resources/add/resource-form-generated.ts
@@ -2,7 +2,10 @@ import {bindable} from "aurelia-templating";
 import {ResourceKind} from "resources-config/resource-kind/resource-kind";
 import {Resource} from "resources/resource";
 import {bindingMode} from "aurelia-binding";
+import {I18N} from "aurelia-i18n";
+import {autoinject} from "aurelia-dependency-injection";
 
+@autoinject
 export class ResourceFormGenerated {
   @bindable
   resourceKind: ResourceKind;
@@ -10,7 +13,13 @@ export class ResourceFormGenerated {
   @bindable({defaultBindingMode: bindingMode.twoWay})
   resource: Resource;
 
+  currentLanguageCode: string;
+
   resourceKindChanged() {
     this.resource.contents = {};
+  }
+
+  constructor(i18n: I18N) {
+    this.currentLanguageCode = i18n.getLocale().toUpperCase();
   }
 }
