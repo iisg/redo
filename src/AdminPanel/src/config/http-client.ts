b58a7bb3e558d2b07ca13fec55f6b170b59d9700
Marcin <nikielmcn@gmail.com>
2017-02-13 11:43:35 +0100

    [REPEKA-105] Global exception handling in frontend
    Change-Id: I7296560bb4447b21ab64778f05bcfd92c7873ef9

--- src/AdminPanel/src/config/http-client.ts
+++ src/AdminPanel/src/config/http-client.ts
@@ -1,9 +1,13 @@
 import {Aurelia} from "aurelia-framework";
 import {HttpClient, HttpResponseMessage, Interceptor, RequestMessage} from "aurelia-http-client";
 import {MetricsSenderInterceptor} from "../common/metrics/metrics-sender-interceptor";
+import * as swal from 'sweetalert2';
+import {I18N} from "aurelia-i18n";
+import {autoinject} from "aurelia-dependency-injection";
 
 export function configure(aurelia: Aurelia) {
   let client: HttpClient = aurelia.container.get(HttpClient);
+  let globalExceptionInterceptor = aurelia.container.get(GlobalExceptionInterceptor);
   client.configure((config) => {
     config
       .withBaseUrl("/api/")
@@ -11,7 +15,8 @@ export function configure(aurelia: Aurelia) {
       .withHeader('X-Requested-With', 'XMLHttpRequest')
       .withInterceptor(new RedirectToLoginIfUnauthenticatedInterceptor())
       .withInterceptor(new MetricsSenderInterceptor())
-      .withInterceptor(new CsrfHeaderInterceptor());
+      .withInterceptor(new CsrfHeaderInterceptor())
+      .withInterceptor(globalExceptionInterceptor);
   });
 }
 
@@ -43,5 +48,40 @@ class CsrfHeaderInterceptor implements Interceptor {
       }
     }
     return request;
+  }
+}
+
+@autoinject
+class GlobalExceptionInterceptor implements Interceptor {
+
+  constructor(private i18n: I18N) {
+  }
+
+  responseError(response: HttpResponseMessage): HttpResponseMessage {
+    let prodAlert = {
+      title: this.i18n.tr("Error") + ' ' + response.content.status,
+      text: this.i18n.tr("exceptions::" + response.content.message),
+      type: "error",
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "OK",
+    };
+
+    let devAlert = {
+      showCancelButton: true,
+      confirmButtonText: this.i18n.tr("Show Profiler"),
+      cancelButtonText: this.i18n.tr("Close"),
+    };
+
+    let isDebug = response.headers.has('X-Debug-Token-Link');
+
+    if (response.statusCode >= 400 && response.statusCode != 401) {
+      swal(isDebug ? $.extend({}, prodAlert, devAlert) : prodAlert).then(() => {
+        if (isDebug) {
+          window.open(response.headers.get('X-Debug-Token-Link'));
+        }
+      });
+    }
+
+    throw response;
   }
 }
