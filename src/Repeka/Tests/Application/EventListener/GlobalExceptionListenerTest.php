e7e36bc922fcc1d44b58df8facdb8569f2d0bc31
Krzysztof Śmiałek <admin@avensome.net>
2017-06-15 15:27:01 +0200

    [REPEKA-165] Log handled exceptions
    Change-Id: If468530a1d0a10a7638b801b511c3a3e7bf9eab3

--- src/Repeka/Tests/Application/EventListener/GlobalExceptionListenerTest.php
+++ src/Repeka/Tests/Application/EventListener/GlobalExceptionListenerTest.php
@@ -1,8 +1,9 @@
 <?php
 namespace Repeka\Tests\Application\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Repeka\Application\Entity\UserEntity;
-use Repeka\Application\EventListener\GlobalExceptionHandler;
+use Repeka\Application\EventListener\GlobalExceptionListener;
 use Repeka\Domain\Exception\DomainException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,66 +14,73 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
-class GlobalExceptionHandlerTest extends \PHPUnit_Framework_TestCase {
-    /** @var  GlobalExceptionHandler */
-    private $handler;
+/** @SuppressWarnings(PHPMD.CouplingBetweenObjects) */
+class GlobalExceptionListenerTest extends \PHPUnit_Framework_TestCase {
+    /** @var  GlobalExceptionListener */
+    private $listener;
 
+    /** @var TokenStorage|\PHPUnit_Framework_MockObject_MockObject */
     private $tokenStorage;
+    /** @var SessionInterface|\PHPUnit_Framework_MockObject_MockObject */
     private $session;
+    /** @var LoggerInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $logger;
 
     public function setUp() {
         $this->tokenStorage = $this->createMock(TokenStorage::class);
         $this->session = $this->createMock(SessionInterface::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
     }
 
     public function testHandleException() {
-        $this->handler = new GlobalExceptionHandler(true, $this->tokenStorage, $this->session);
+        $this->listener = new GlobalExceptionListener(true, $this->tokenStorage, $this->session, $this->logger);
         $mockedEvent = $this->createMock(GetResponseForExceptionEvent::class);
         $mockedEvent->expects($this->once())->method('getException')->willReturn(new DomainException('a'));
         $mockedEvent->expects($this->once())->method('setResponse');
         $mockedEvent->method('getRequest')->willReturn($this->createMock(Request::class));
-        $this->handler->onException($mockedEvent);
+        $this->logger->expects($this->once())->method('error');
+        $this->listener->onException($mockedEvent);
     }
 
-    public function testHandleDomainException() {
-        $this->handler = new GlobalExceptionHandler(false, $this->tokenStorage, $this->session);
-        $response = $this->handler->createErrorResponse(new DomainException('Error'), new Request());
+    public function testDomainExceptionResponse() {
+        $this->listener = new GlobalExceptionListener(false, $this->tokenStorage, $this->session, $this->logger);
+        $response = $this->listener->createErrorResponse(new DomainException('Error'), new Request());
         $expectedResponse = new JsonResponse(['message' => 'Error',], 400);
         $this->assertEquals($expectedResponse, $response);
     }
 
-    public function testHandleAuthenticationMissingException() {
+    public function testAuthenticationMissingExceptionResponse() {
         $mockedUser = $this->createMock(TokenInterface::class);
         $mockedUser->method('getUser')->willReturn(null);
         $mockedToken = $this->createMock(TokenStorage::class);
         $mockedToken->method('getToken')->willReturn($mockedUser);
-        $this->handler = new GlobalExceptionHandler(false, $mockedToken, $this->session);
-        $response = $this->handler->createErrorResponse(new AuthenticationException(), new Request());
+        $this->listener = new GlobalExceptionListener(false, $mockedToken, $this->session, $this->logger);
+        $response = $this->listener->createErrorResponse(new AuthenticationException(), new Request());
         $expectedResponse = new JsonResponse(['message' => 'Unauthorized'], 401);
         $this->assertEquals($expectedResponse, $response);
     }
 
-    public function testHandleAccessDeniedException() {
+    public function testAccessDeniedExceptionResponse() {
         $mockedUser = $this->createMock(TokenInterface::class);
         $mockedUser->method('getUser')->willReturn($this->createMock(UserEntity::class));
         $mockedToken = $this->createMock(TokenStorage::class);
         $mockedToken->method('getToken')->willReturn($mockedUser);
-        $this->handler = new GlobalExceptionHandler(false, $mockedToken, $this->session);
-        $response = $this->handler->createErrorResponse(new AccessDeniedException('Forbidden'), new Request());
+        $this->listener = new GlobalExceptionListener(false, $mockedToken, $this->session, $this->logger);
+        $response = $this->listener->createErrorResponse(new AccessDeniedException('Forbidden'), new Request());
         $expectedResponse = new JsonResponse(['message' => 'Forbidden'], 403);
         $this->assertEquals($expectedResponse, $response);
     }
 
-    public function testHandleOtherExceptions() {
-        $this->handler = new GlobalExceptionHandler(false, $this->tokenStorage, $this->session);
-        $response = $this->handler->createErrorResponse(new \Exception('ExceptionError'), new Request());
+    public function testOtherExceptionResponses() {
+        $this->listener = new GlobalExceptionListener(false, $this->tokenStorage, $this->session, $this->logger);
+        $response = $this->listener->createErrorResponse(new \Exception('ExceptionError'), new Request());
         $expectedResponse = new JsonResponse(['message' => 'Internal server error.'], 500);
         $this->assertEquals($expectedResponse, $response);
     }
 
     public function testDisplayingOtherExceptionMessageIfDebug() {
-        $this->handler = new GlobalExceptionHandler(true, $this->tokenStorage, $this->session);
-        $response = $this->handler->createErrorResponse(new \Exception('ExceptionError'), new Request());
+        $this->listener = new GlobalExceptionListener(true, $this->tokenStorage, $this->session, $this->logger);
+        $response = $this->listener->createErrorResponse(new \Exception('ExceptionError'), new Request());
         $expectedResponse = new JsonResponse(['message' => 'ExceptionError'], 500);
         $this->assertEquals($expectedResponse, $response);
     }
--- web/app.php
+++ web/app.php
@@ -1,9 +1,9 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
 ini_set("log_errors", true);
-ini_set("error_log", __DIR__ . "/../var/logs/error.log");
+ini_set("error_log", __DIR__ . "/../var/logs/php_error.log");
 defined('REPEKA_ENV') || define('REPEKA_ENV', (getenv('REPEKA_ENV') ?: 'prod'));
 $loader = require __DIR__ . '/../app/autoload.php';
 if (REPEKA_ENV === 'prod') {
