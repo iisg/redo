cb3662fe694fcc5faa398b01eb1a1761585a3e57
Krzysztof Śmiałek <admin@avensome.net>
2017-07-26 13:35:05 +0200

    [REPEKA-202] Fix locale problems when app is bundled
    Change-Id: I34638fc95ab93e781c835e4f8c50af93d1a91aa7
    Reviewed-on: https://gerrit.fslab.agh.edu.pl/6989
    Reviewed-by: Wojciech Frącz <fraczwojciech@gmail.com>
    Tested-by: Jenkins Apps

--- src/AdminPanel/build/tasks/bundle.js
+++ src/AdminPanel/build/tasks/bundle.js
@@ -1,22 +1,30 @@
-var gulp = require('gulp');
-var bundler = require('aurelia-bundler');
-var bundles = require('../bundles.js');
-var paths = require('../paths');
-var runSequence = require('run-sequence');
-var path = require('path');
+'use strict';
 
-var config = {
+const gulp = require('gulp');
+const bundler = require('aurelia-bundler');
+const bundles = require('../bundles.js');
+const fs = require('fs');
+const path = require('path');
+const runSequence = require('run-sequence');
+
+const paths = require('../paths');
+
+const config = {
   force: true,
   baseURL: paths.webRoot,
   configPath: path.join(paths.webAdminRoot, 'jspm.config.js'),
   bundles: bundles.bundles
 };
 
-var calledBuild = false;
+let calledBuild = false;
 
 // does not have build as dependency in order to allow bundling of already built sources
 gulp.task('bundle', function () {
   try {
+    const locales = fs.readdirSync(paths.locales)
+      .filter(file => fs.lstatSync(path.join(paths.locales, file)).isDirectory());
+    //noinspection NodeModulesDependencies,ES6ModulesDependencies because JSON is a global builtin
+    fs.writeFileSync(path.join(paths.webAdminRoot, 'locales.json'), JSON.stringify(locales));
     return bundler.bundle(config);
   } catch (e) {
     if (calledBuild) {
