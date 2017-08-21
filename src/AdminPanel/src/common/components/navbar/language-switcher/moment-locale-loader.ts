153c3e2999a5403d0bc907aaa89f42b5d931c81c
Krzysztof Śmiałek <admin@avensome.net>
2017-08-21 13:51:11 +0200

    [REPEKA-228] Fix inability to change language from English
    Change-Id: I8a466681f30490212c7c518a07b01bc6284ba740
    Reviewed-on: https://gerrit.fslab.agh.edu.pl/7043
    Reviewed-by: Wojciech Frącz <fraczwojciech@gmail.com>
    Tested-by: Jenkins Apps

--- src/AdminPanel/src/common/components/navbar/language-switcher/moment-locale-loader.ts
+++ src/AdminPanel/src/common/components/navbar/language-switcher/moment-locale-loader.ts
@@ -1,13 +1,21 @@
 export class MomentLocaleLoader {
   load(locale: string): Promise<any> {
-    return SystemJS.import(this.getLocaleModule(locale))
-      .catch(() => {
-        const simplified = this.simplify(locale);
-        return SystemJS.import(this.getLocaleModule(simplified));
-      });
+    if (locale = 'en') {
+      return Promise.resolve(); // moment's default locale
+    }
+    return this.loadLocaleModule(locale).catch(() => {
+      const simplified = this.simplify(locale);
+      return (simplified != locale)
+        ? this.loadLocaleModule(simplified).catch(() => Promise.resolve())
+        : Promise.resolve();
+    });
   }
 
-  private getLocaleModule(locale: string): string {
+  private loadLocaleModule(locale: string): Promise<any> {
+    return SystemJS.import(this.getLocaleModuleName(locale));
+  }
+
+  private getLocaleModuleName(locale: string): string {
     return `moment/locale/${locale}`;
   }
 
