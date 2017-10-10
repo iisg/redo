5b038060ce98353571b79983d448e0504c27b607
Krzysztof Śmiałek <admin@avensome.net>
2017-10-10 12:23:02 +0200

    [REPEKA-232] Fix workflow graph misclicks
    Change-Id: I8753a1fc2eff05fcfaffb143a8bdbf313e604980
    Reviewed-on: https://gerrit.fslab.agh.edu.pl/7155
    Reviewed-by: Wojciech Frącz <fraczwojciech@gmail.com>
    Tested-by: Jenkins Apps

--- src/AdminPanel/src/workflows/details/graph/workflow-graph-display.ts
+++ src/AdminPanel/src/workflows/details/graph/workflow-graph-display.ts
@@ -4,28 +4,35 @@ import {WorkflowGraph} from "./workflow-graph";
 import {inject, NewInstance} from "aurelia-dependency-injection";
 import {booleanAttribute} from "common/components/boolean-attribute";
 import {WorkflowGraphReady} from "./workflow-graph-events";
+import {WorkflowGraphManager} from "./workflow-graph-manager";
 
-@inject(NewInstance.of(WorkflowGraph), Element)
+@inject(NewInstance.of(WorkflowGraph), Element, WorkflowGraphManager)
 export class WorkflowGraphDisplay implements ComponentAttached, ComponentDetached {
   diagramContainer: HTMLElement;
   @bindable workflow: Workflow;
   @bindable current: WorkflowPlace[];
   @bindable @booleanAttribute editable: boolean = false;
 
-  constructor(private graph: WorkflowGraph, private element: Element) {
+  constructor(private graph: WorkflowGraph, private element: Element, private graphManager: WorkflowGraphManager) {
   }
 
   attached() {
     this.graph.render(this.workflow, this.diagramContainer, this.editable);
     this.currentChanged();
     this.element.dispatchEvent(WorkflowGraphReady.newInstance(this.graph));
+    this.graphManager.register(this);
   }
 
   detached(): void {
+    this.graphManager.unregister(this);
     this.graph.destroy();
   }
 
   currentChanged() {
     this.graph.ready.then(() => this.graph.highlightCurrent(this.current));
+  }
+
+  recalculateGraphPosition(): void {
+    this.graph.recalculatePosition();
   }
 }
--- src/AdminPanel/src/workflows/details/workflow-edit-form.ts
+++ src/AdminPanel/src/workflows/details/workflow-edit-form.ts
@@ -6,6 +6,7 @@ import {autoinject} from "aurelia-dependency-injection";
 import {DOM} from "aurelia-framework";
 import {WorkflowGraphEditorReady} from "./graph/workflow-graph-events";
 import {WorkflowGraphEditor} from "./graph/workflow-graph-editor";
+import {WorkflowGraphManager} from "./graph/workflow-graph-manager";
 
 @autoinject
 export class WorkflowEditForm {
@@ -13,9 +14,15 @@ export class WorkflowEditForm {
   private controller: ValidationController;
   private editor: WorkflowGraphEditor;
 
-  constructor(validationControllerFactory: ValidationControllerFactory, private element: Element) {
+  constructor(validationControllerFactory: ValidationControllerFactory,
+              private element: Element,
+              private graphManager: WorkflowGraphManager) {
     this.controller = validationControllerFactory.createForCurrentScope();
     this.controller.addRenderer(new BootstrapValidationRenderer());
+  }
+
+  updateGraphPosition(): void {
+    this.graphManager.forEach(graph => graph.recalculateGraphPosition());
   }
 
   onEditorReady(event: WorkflowGraphEditorReady) {
