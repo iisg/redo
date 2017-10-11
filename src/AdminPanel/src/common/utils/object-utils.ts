7eff67ae90b749ee9af019041f75b16038029a5f
Krzysztof Śmiałek <admin@avensome.net>
2017-10-11 16:09:42 +0200

    [REPEKA-254] Fix reading restricting metadata in frontend
    Change-Id: I3ee45f0686d527bf2c72ed6241969ed1073d9aa8
    Reviewed-on: https://gerrit.fslab.agh.edu.pl/7171
    Tested-by: Jenkins Apps
    Reviewed-by: Wojciech Frącz <fraczwojciech@gmail.com>

--- src/AdminPanel/src/common/utils/object-utils.ts
+++ src/AdminPanel/src/common/utils/object-utils.ts
@@ -7,6 +7,10 @@ export function isObject(object: any): boolean {
   return typeof object == 'object';
 }
 
-export function keysByValue<M, V>(obj: M, value: V): any[] {
+export function keysByValue<M, V>(obj: M, value: V): string[] {
   return Object.keys(obj).filter(key => obj[key] === value);
+}
+
+export function numberKeysByValue<M, V>(obj: M, value: V): number[] {
+  return keysByValue(obj, value).map(key => parseInt(key, 10)).filter(val => !Number.isNaN(val));
 }
