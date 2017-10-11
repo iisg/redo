cd2f84080cc8d1af5a45d6b32ae398f484a96878
Krzysztof Śmiałek <admin@avensome.net>
2017-10-11 17:38:26 +0200

    [REPEKA-255] Fix missing tasks again
    Change-Id: Iff3b3b73e4ea651e2826ae1aa5a5df7dfb3aebd3
    Reviewed-on: https://gerrit.fslab.agh.edu.pl/7172
    Tested-by: Jenkins Apps
    Reviewed-by: Wojciech Frącz <fraczwojciech@gmail.com>

--- src/AdminPanel/src/home.ts
+++ src/AdminPanel/src/home.ts
@@ -8,16 +8,9 @@ export class Home implements ConfiguresRouter {
   configureRouter(config: RouterConfiguration, router: Router): void {
     this.router = router;
     config.map([
-      {
-        route: '',
-        name: 'home',
-        moduleId: 'tasks/tasks'
-      },
-      {
-        route: 'resources/:id',
-        name: 'resources/details',
-        moduleId: 'resources/details/resource-details',
-      },
+      {route: '', name: 'home', moduleId: 'tasks/tasks'},
+      {route: 'resources/:id', name: 'resources/details', moduleId: 'resources/details/resource-details'},
+      {route: 'users/:id', name: 'users/details', moduleId: 'users/details/user-details'},
     ]);
   }
 }
