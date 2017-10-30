c801e2b846e7c00587bbd9163d0bbcb622729027
Wojciech Frącz <fraczwojciech@gmail.com>
2017-10-30 11:58:20 +0100

    [REPEKA-275] Extract separate command for ensuring db connection
    Change-Id: I3b1653da2b382acd589df7f7cba819790630a6f0
    Reviewed-on: https://gerrit.fslab.agh.edu.pl/7262
    Reviewed-by: Wojciech Frącz <fraczwojciech@gmail.com>
    Tested-by: Jenkins Apps

---
+++ src/Repeka/Application/Command/CheckDatabaseConnectionCommand.php
@@ -0,0 +1,36 @@
+<?php
+namespace Repeka\Application\Command;
+
+use Doctrine\Bundle\DoctrineBundle\Command\DoctrineCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CheckDatabaseConnectionCommand extends DoctrineCommand {
+    protected function configure() {
+        $this
+            ->setName('repeka:prepare-database')
+            ->setDescription('Creates a database for application as soon as the server is available.');
+    }
+
+    /** @inheritdoc */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $this->getApplication()->setAutoExit(false);
+        $this->getApplication()->setCatchExceptions(true);
+        $dbConnectionRetries = 10;
+        $createDbCommand = new StringInput('doctrine:database:create --if-not-exists --no-interaction');
+        do {
+            $connectionStatus = $this->getApplication()->run($createDbCommand, $dbConnectionRetries ? new NullOutput() : $output);
+            if ($connectionStatus !== 0) {
+                if ($dbConnectionRetries <= 0) {
+                    throw new \RuntimeException('Could not connect to to the database.');
+                } else {
+                    $output->writeln("Waiting for database connection ($dbConnectionRetries)...");
+                    --$dbConnectionRetries;
+                    sleep(3);
+                }
+            }
+        } while ($connectionStatus !== 0);
+    }
+}
--- src/Repeka/Application/Command/Initialization/InitializeApplicationCommand.php
+++ src/Repeka/Application/Command/Initialization/InitializeApplicationCommand.php
@@ -1,8 +1,6 @@
 <?php
 namespace Repeka\Application\Command\Initialization;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Elastica\Exception\RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\StringInput;
@@ -19,9 +17,8 @@ class InitializeApplicationCommand extends ContainerAwareCommand {
     /** @inheritdoc */
     protected function execute(InputInterface $input, OutputInterface $output) {
         $this->getApplication()->setAutoExit(false);
+        $this->getApplication()->run(new StringInput('repeka:prepare-database'), $output);
         $this->getApplication()->setCatchExceptions(false);
-        $this->waitForDatabaseConnection($output);
-        $this->getApplication()->run(new StringInput('doctrine:database:create --if-not-exists --no-interaction'), $output);
         if (!$input->getOption('skip-backup')) {
             $this->getApplication()->run(new StringInput('db:backup'), $output);
         }
@@ -31,27 +28,5 @@ class InitializeApplicationCommand extends ContainerAwareCommand {
         $this->getApplication()->run(new StringInput('repeka:initialize:system-resource-kinds'), $output);
         $this->getApplication()->run(new StringInput('repeka:initialize:system-resource-kinds-metadata'), $output);
         $this->getApplication()->run(new StringInput('repeka:initialize:system-user-roles'), $output);
-    }
-
-    private function waitForDatabaseConnection(OutputInterface $output): void {
-        $dbConnectionRetries = 5;
-        while (!$this->hasDatabaseConnection()) {
-            if ($dbConnectionRetries <= 0) {
-                throw new RuntimeException('Could not connecto to the database.');
-            } else {
-                $output->writeln("Waiting for database connection ($dbConnectionRetries)...");
-                --$dbConnectionRetries;
-                sleep(1);
-            }
-        }
-    }
-
-    private function hasDatabaseConnection(): bool {
-        try {
-            $this->getContainer()->get(EntityManagerInterface::class)->getConnection()->connect();
-            return true;
-        } catch (\Exception $e) {
-            return false;
-        }
     }
 }
