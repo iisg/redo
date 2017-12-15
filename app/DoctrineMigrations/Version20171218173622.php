6f74cd909f176b78df22259d09926b5e3f7f07fd
karolkozak <karol2868@gmail.com>
2017-12-15 14:59:52 +0100

    [REPEKA-310] Fix metadata maxCount validation
    Change-Id: I6f2f3f6775ca2c5d09d0b25b6f9a35a386799683

--- app/DoctrineMigrations/Version20171218173622.php
+++ app/DoctrineMigrations/Version20171218173622.php
@@ -7,26 +7,33 @@ use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 /**
- * Adds count constraints
+ * Adds maxCount constraints
  */
-class Version20171016130000 extends AbstractMigration implements ContainerAwareInterface {
+class Version20171218173622 extends AbstractMigration implements ContainerAwareInterface {
     use ContainerAwareTrait;
 
     public function up(Schema $schema) {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
         $this->addSql(<<<SQL
 UPDATE metadata
-SET constraints = '{"count": {"min": 1, "max": 1}}'::JSONB || CASE
+SET constraints = jsonb_build_object('maxCount', constraints #> '{count,max}') || constraints
+WHERE JSONB_EXISTS(constraints, 'count') = TRUE
+SQL
+        );
+        $this->addSql("UPDATE metadata SET constraints = constraints - 'count'");
+        $this->addSql(<<<SQL
+UPDATE metadata
+SET constraints = '{"maxCount": 0}'::JSONB || CASE
   WHEN constraints = '[]'::JSONB THEN '{}'::JSONB -- handle empty objects stored as arrays
   ELSE constraints
 END
-WHERE resource_kind_id IS NULL
+WHERE resource_kind_id IS NULL AND JSONB_EXISTS(constraints, 'maxCount') = FALSE
 SQL
         );
     }
 
     public function down(Schema $schema) {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-        $this->addSql("UPDATE metadata SET constraints = constraints - 'count'");
+        $this->addSql("UPDATE metadata SET constraints = constraints - 'maxCount'");
     }
 }
