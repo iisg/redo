fa9eb0af76266efdb3e0f3b3da3259699dc8428f
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-15 11:32:39 +0100

    [NT] Make generate-self-signed-certs work in Git Bash
    Change-Id: I3054ec1e5b9ef85627ff1eef03da103db49192b6

--- var/ssl/generate-self-signed-certs.sh
+++ var/ssl/generate-self-signed-certs.sh
@@ -2,4 +2,10 @@
 
 cd "$(dirname "$0")"
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt -subj "/C=PL/ST=Repeka/L=Repeka/O=Dis/CN=repeka"
+if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    export SUBJECT="//C=PL\ST=REPEKA\L=REPEKA\O=Dis\CN=REPEKA"
+else
+    export SUBJECT="/C=PL/ST=REPEKA/L=REPEKA/O=Dis/CN=REPEKA"
+fi
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt -subj $SUBJECT
