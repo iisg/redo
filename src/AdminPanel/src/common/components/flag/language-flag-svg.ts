0f90390a4d6e732483544a0a3a079a26438e2e56
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-18 11:15:48 +0100

    [NT] Fix region-flags jspm config, update statsd
    https://github.com/behdad/region-flags/issues/33
    
    Change-Id: I0ac9f20064f26ab60e0b44de9ebdb0f93cc37844

--- src/AdminPanel/src/common/components/flag/language-flag-svg.ts
+++ src/AdminPanel/src/common/components/flag/language-flag-svg.ts
@@ -1,7 +1,7 @@
 export class LanguageFlagSvgValueConverter implements ToViewValueConverter {
   toView(flag: string): string {
     if (flag) {
-      return `/jspm_packages/github/behdad/region-flags@1.0.1/svg/${flag.toUpperCase()}.svg`;
+      return `/jspm_packages/npm/region-flags@1.1.0/svg/${flag.toUpperCase()}.svg`;
     } else {
       return '/files/dummy-flag.svg';
     }
