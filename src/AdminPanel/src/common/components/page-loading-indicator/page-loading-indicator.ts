2c64aaa5ea5e9a24eced566e28e5ddcdb267c908
mkasprz <kasprzyk@protonmail.com>
2018-01-28 20:05:37 +0100

    [REPEKA-319] Page loading indicator
    Change-Id: I64f3292533eb1e2486776a7077b099ac1f3f08ce

---
+++ src/AdminPanel/src/common/components/page-loading-indicator/page-loading-indicator.ts
@@ -0,0 +1,19 @@
+import * as nprogress from "nprogress";
+import { noView, bindable } from "aurelia-framework";
+
+@noView(['nprogress/nprogress.css'])
+export class PageLoadingIndicator {
+    @bindable loading = false;
+
+    attached() {
+        NProgress.configure({parent: 'page-loading-indicator'});
+    }
+
+    loadingChanged(newValue: boolean) {
+        if (newValue) {
+            nprogress.start();
+        } else {
+            nprogress.done();
+        }
+    }
+}
