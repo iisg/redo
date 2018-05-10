bfe962a45dbe4b5b5fad1848f9464f59388b614b
Pawel Grochola <grocholapawel@gmail.com>
2018-05-10 10:08:24 +0200

    [REPEKA-408] Create page for resource kind details
    Change-Id: I9370a33c5ea9e433a4da2f251cc211c693119f7b

--- src/AdminPanel/src/resources-config/resource-kind/resource-kind-view.ts
+++ src/AdminPanel/src/resources-config/resource-kind/resource-kind-view.ts
@@ -6,6 +6,7 @@ export class ResourceKindView implements ConfiguresRouter {
   configureRouter(config: RouterConfiguration, router: Router): void {
     config.map([
       {route: '', name: 'resource-kinds/list', moduleId: './resource-kinds-list'},
+      {route: '/details/:id', name: 'resource-kinds/details', moduleId: './details/resource-kind-details'},
       {route: '/../workflows/:id', name: 'workflows/details', moduleId: 'workflows/details/workflow-details'},
     ]);
   }
--- src/AdminPanel/src/resources-config/resource-kind/resource-kinds-list.ts
+++ src/AdminPanel/src/resources-config/resource-kind/resource-kinds-list.ts
@@ -2,8 +2,6 @@ import {ResourceKindRepository} from "./resource-kind-repository";
 import {autoinject} from "aurelia-dependency-injection";
 import {ResourceKind} from "./resource-kind";
 import {InCurrentLanguageValueConverter} from "../multilingual-field/in-current-language";
-import {Alert} from "common/dialog/alert";
-import {DeleteEntityConfirmation} from "common/dialog/delete-entity-confirmation";
 import {ContextResourceClass} from 'resources/context/context-resource-class';
 
 @autoinject
@@ -14,9 +12,6 @@ export class ResourceKindsList {
   resourceKinds: ResourceKind[];
 
   constructor(private resourceKindRepository: ResourceKindRepository,
-              private inCurrentLanguage: InCurrentLanguageValueConverter,
-              private alert: Alert,
-              private deleteEntityConfirmation: DeleteEntityConfirmation,
               private contextResourceClass: ContextResourceClass) {
   }
 
@@ -48,26 +43,7 @@ export class ResourceKindsList {
     });
   }
 
-  displayWorkflowPreview(resourceKind: ResourceKind): Promise<any> {
-    const title = this.inCurrentLanguage.toView(resourceKind.workflow.name);
-    return this.alert.show({imageUrl: resourceKind.workflow.thumbnail}, title);
-  }
-
-  saveEditedResourceKind(resourceKind: ResourceKind, changedResourceKind: ResourceKind): Promise<any> {
-    resourceKind.pendingRequest = true;
-    return this.resourceKindRepository.update(changedResourceKind)
-      .then(updated => $.extend(resourceKind, updated))
-      .finally(() => resourceKind.pendingRequest = false);
-  }
-
-  deleteResourceKind(resourceKind: ResourceKind): Promise<any> {
-    return this.deleteEntityConfirmation.confirm('resourceKind', resourceKind.id)
-      .then(() => resourceKind.pendingRequest = true)
-      .then(() => this.resourceKindRepository.remove(resourceKind))
-      .then(() => {
-        const index = this.resourceKinds.findIndex(rk => rk.id == resourceKind.id);
-        this.resourceKinds.splice(index, 1);
-      })
-      .finally(() => resourceKind.pendingRequest = false);
+  toggleEditForm() {
+    this.addFormOpened = !this.addFormOpened;
   }
 }
