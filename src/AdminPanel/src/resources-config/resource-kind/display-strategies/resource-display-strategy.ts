6925255ab35450c5770d9bf064e69d40376d7825
Pawel Grochola <grocholapawel@gmail.com>
2018-05-16 17:57:04 +0200

    [REPEKA-466] Fix empty resource display strategy
    Change-Id: If0379e0ae737dbf8e5e6b29ee9117e06bb429dcd

--- src/AdminPanel/src/resources-config/resource-kind/display-strategies/resource-display-strategy.ts
+++ src/AdminPanel/src/resources-config/resource-kind/display-strategies/resource-display-strategy.ts
@@ -1,9 +1,13 @@
 import {Resource} from "../../../resources/resource";
 import {autoinject} from "aurelia-dependency-injection";
+import {I18N} from "aurelia-i18n";
 
 @autoinject
 export class ResourceDisplayStrategyValueConverter implements ToViewValueConverter {
+  constructor(private i18n: I18N) {
+  }
+
   toView(resource: Resource, displayStrategyId: string): any {
-    return resource.displayStrategies[displayStrategyId] || '';
+    return resource.displayStrategies[displayStrategyId] || this.i18n.tr('Resource') + ` #${resource.id}`;
   }
 }
