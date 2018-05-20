8ece1077fdddbc7924b21ca8d8f587658cf28db6
skrzypul <blazej.skrzypulec@gmail.com>
2018-05-20 21:52:59 +0200

    [REPEKA-497] Add protractor login tests
    Change-Id: If717ac3d2b521e2cb97a70390aa43c2d7e4d638c

---
+++ src/AdminPanel/tests/e2e/protractor.config.js
@@ -0,0 +1,17 @@
+exports.config = {
+  framework: 'jasmine',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['LoginTests.js'],
+  getPageTimeout: 120000,
+  allScriptsTimeout: 120000,
+  multiCapabilities: [
+    {
+      'browserName': 'chrome'
+    }
+  ],
+
+  onPrepare: function () {
+    browser.ignoreSynchronization = true;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
+  },
+};
