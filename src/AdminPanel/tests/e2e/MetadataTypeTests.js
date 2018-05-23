244c59ea48dde0e508b2f8c59c00e667403ff89d
skrzypul <blazej.skrzypulec@gmail.com>
2018-05-23 19:01:25 +0200

    [REPEKA-498] Add basic metadata type tests
    Change-Id: I9a714d8bb673e5ebaf4e68ac02913706ad3a7387

---
+++ src/AdminPanel/tests/e2e/MetadataTypeTests.js
@@ -0,0 +1,160 @@
+describe('Metadata Type Tests', function() {
+	
+  function waitForElement (locator) {  
+	   browser.driver.wait(function () {
+			return locator.isPresent()
+	   }, 10000);
+  }
+
+  beforeAll(function() {
+		browser.get('https://repekadev.fslab.agh.edu.pl/');	
+		
+		var loginButton = element(by.linkText('Zaloguj się'));
+	    loginButton.click();
+	  
+	    var nameField = element(by.id('username'));
+	    var passwordField = element(by.id('password'));
+	    var loginIcons = element.all(by.className('login-box-icon'));
+	    nameField.clear();
+		passwordField.clear();
+		nameField.sendKeys('admin');
+	    passwordField.sendKeys('admin');
+	    loginIcons.get(2).click();
+  });
+  
+  beforeEach(function() {
+		browser.get('https://repekadev.fslab.agh.edu.pl/admin/metadata/books');
+  });
+  
+  it('Try to add without name', function() {
+	    waitForElement(element(by.buttonText('Dodaj rodzaj metadanej'))); 
+		
+		var addMetadataTypeButton = element(by.buttonText('Dodaj rodzaj metadanej'));
+		addMetadataTypeButton.click();
+		
+		var fields = element.all(by.className('form-control au-target'));
+		var addButton = element(by.buttonText('Dodaj'));
+		fields.get(2).sendKeys('Nazwa_Testowanie_Automatyczne');
+		fields.get(3).sendKeys('Nazwa_Testowanie_Automatyczne');
+		addButton.click();
+		
+		var errorMessage = element(by.className('help-block validation-message'));
+		expect(errorMessage.getText()).toContain('To pole jest wymagane.');
+  });
+  
+  it('Try to add without Polish display name', function() {
+	    waitForElement(element(by.buttonText('Dodaj rodzaj metadanej'))); 
+		
+		var addMetadataTypeButton = element(by.buttonText('Dodaj rodzaj metadanej'));
+		addMetadataTypeButton.click();
+		
+		var fields = element.all(by.className('form-control au-target'));
+		var addButton = element(by.buttonText('Dodaj'));
+		fields.get(0).sendKeys('Nazwa_Testowanie_Automatyczne');
+		fields.get(3).sendKeys('Nazwa_Testowanie_Automatyczne');
+		addButton.click();
+		
+		var errorMessage = element(by.className('help-block validation-message'));
+		expect(errorMessage.getText()).toContain('Nazwa wyświetlana musi mieć wartość we wszystkich językach.');
+  });
+  
+  it('Try to add without English display name', function() {
+	    waitForElement(element(by.buttonText('Dodaj rodzaj metadanej'))); 
+		
+		var addMetadataTypeButton = element(by.buttonText('Dodaj rodzaj metadanej'));
+		addMetadataTypeButton.click();
+		
+		var fields = element.all(by.className('form-control au-target'));
+		var addButton = element(by.buttonText('Dodaj'));
+		fields.get(0).sendKeys('Nazwa_Testowanie_Automatyczne');
+		fields.get(2).sendKeys('Nazwa_Testowanie_Automatyczne');
+		addButton.click();
+		
+		var errorMessage = element(by.className('help-block validation-message'));
+		expect(errorMessage.getText()).toContain('Nazwa wyświetlana musi mieć wartość we wszystkich językach.');
+  });
+  
+  it('Add metadata type', function() {
+	    waitForElement(element(by.buttonText('Dodaj rodzaj metadanej'))); 
+		
+		var addMetadataTypeButton = element(by.buttonText('Dodaj rodzaj metadanej'));
+		addMetadataTypeButton.click();
+		
+		var fields = element.all(by.className('form-control au-target'));
+		var addButton = element(by.buttonText('Dodaj'));
+		fields.get(0).sendKeys('Nazwa_Testowanie_Automatyczne');
+		fields.get(2).sendKeys('Nazwa_Testowanie_Automatyczne');
+		fields.get(3).sendKeys('Nazwa_Testowanie_Automatyczne');
+		addButton.click();
+		
+	    waitForElement(element(by.linkText('Nazwa_Testowanie_Automatyczne'))); 
+		expect(element(by.linkText('Nazwa_Testowanie_Automatyczne')).isPresent()).toBe(true);
+  });
+
+  it('Edit metadata Polish display name', function() {
+		waitForElement(element(by.linkText('Nazwa_Testowanie_Automatyczne')));
+		var newMetadataTypeRowLink = element(by.linkText('Nazwa_Testowanie_Automatyczne'));
+		newMetadataTypeRowLink.click();
+		
+		waitForElement(element(by.buttonText('Edytuj')));
+		var editButton = element(by.buttonText('Edytuj'));
+		editButton.click();
+		
+		waitForElement(element(by.className('form-control au-target')));
+		var fields = element.all(by.className('form-control au-target'));
+		fields.get(2).clear();
+		fields.get(2).sendKeys('Edycja_Nazwy');
+		var confirmButton = element(by.buttonText('Zatwierdź'));
+		confirmButton.click();
+		
+		waitForElement(element(by.className('dl-horizontal')));
+		var parametersList = element(by.className('dl-horizontal'));
+		expect(parametersList.getText()).toContain('Edycja_Nazwy');
+  });
+  
+  // Nie działa z powodu REPEKA-514
+  it('Edit metadata English display name', function() {
+		waitForElement(element(by.linkText('Nazwa_Testowanie_Automatyczne')));
+		var newMetadataTypeRowLink = element(by.linkText('Nazwa_Testowanie_Automatyczne'));
+		newMetadataTypeRowLink.click();
+		
+		waitForElement(element(by.buttonText('Edytuj')));
+		var editButton = element(by.buttonText('Edytuj'));
+		editButton.click();
+		
+		waitForElement(element(by.className('form-control au-target')));
+		var fields = element.all(by.className('form-control au-target'));
+		fields.get(3).clear();
+		fields.get(3).sendKeys('Edycja_Nazwy');
+		
+		var confirmButton = element(by.buttonText('Zatwierdź'));
+		confirmButton.click();
+		
+		waitForElement(element(by.buttonText('Edytuj')));
+		var languageMenu = element(by.className('au-target flag-icon-xs'));
+		languageMenu.click();
+		
+		var englishOption = element(by.linkText('English'));
+		englishOption.click();
+		
+		waitForElement(element(by.className('dl-horizontal')));
+		var parametersList = element(by.className('dl-horizontal'));
+		expect(parametersList.getText()).toContain('Edycja_Nazwy');
+  });
+
+  it('Delete metadata type', function() {
+		waitForElement(element(by.linkText('Nazwa_Testowanie_Automatyczne')));
+		var newMetadataTypeRowLink = element(by.linkText('Nazwa_Testowanie_Automatyczne'));
+		newMetadataTypeRowLink.click();
+		
+		waitForElement(element(by.buttonText('Remove')));
+		var deleteButton = element(by.buttonText('Remove'));
+		deleteButton.click();
+		
+		var confirmButton = element.all(by.className('swal2-confirm btn btn-danger'));
+		confirmButton.click();
+		
+		waitForElement(element(by.buttonText('Add metadata')));
+		expect(element(by.linkText('Nazwa_Testowanie_Automatyczne')).isPresent()).toBe(false);
+  });
+});
