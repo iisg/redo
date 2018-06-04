527d82a3b55a1fac73800c6dbceb010b7f0f4c0e
Wojciech Frącz <fraczwojciech@gmail.com>
2018-06-04 22:17:06 +0200

    [NT] Map all plugins configs as objects if empty
    Change-Id: I0ac8a69054f6b73ceec78d4ad55b6b581e750df3

--- src/Repeka/Application/Serialization/ResourceWorkflowPlaceNormalizer.php
+++ src/Repeka/Application/Serialization/ResourceWorkflowPlaceNormalizer.php
@@ -6,12 +6,26 @@ use Repeka\Domain\Entity\Workflow\ResourceWorkflowPlace;
 class ResourceWorkflowPlaceNormalizer extends LabeledNormalizer {
     public function normalize($place, $format = null, array $context = []) {
         $data = parent::normalize($place, $format, $context);
-        $data['pluginsConfig'] = $this->emptyArrayAsObject($data['pluginsConfig']);
+        $data['pluginsConfig'] = $this->getPluginsConfig($data);
         return $data;
     }
 
     /** @inheritdoc */
     public function supportsNormalization($data, $format = null) {
         return $data instanceof ResourceWorkflowPlace;
+    }
+
+    private function getPluginsConfig(array $data) {
+        $configs = array_map(
+            function ($element) {
+                if (is_array($element)) {
+                    return $this->emptyArrayAsObject($element);
+                } else {
+                    return $element;
+                }
+            },
+            $data['pluginsConfig']
+        );
+        return $this->emptyArrayAsObject($configs);
     }
 }
