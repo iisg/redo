2e01837681dda8d6500be5f1b90491fcf8471425
Wojciech Frącz <fraczwojciech@gmail.com>
2018-06-05 09:08:09 +0200

    [REPEKA-533] Show resource kind in audit resource details
    Change-Id: I305b766709f3a085869dce5d4c7e795f40b42024

--- src/AdminPanel/src/audit/entry-details/audit-entry-details-resource.ts
+++ src/AdminPanel/src/audit/entry-details/audit-entry-details-resource.ts
@@ -4,8 +4,6 @@ import {Modal} from "common/dialog/modal";
 import {Resource} from "../../resources/resource";
 import {AuditEntry} from "../audit-entry";
 import {AuditEntryDetailsResourceContentsModal} from "./audit-entry-details-resource-contents-modal";
-import {MetadataRepository} from "../../resources-config/metadata/metadata-repository";
-import {propertyKeys} from "../../common/utils/object-utils";
 import {ResourceKind} from "../../resources-config/resource-kind/resource-kind";
 
 @autoinject
@@ -13,8 +11,7 @@ export class AuditEntryDetailsResource {
   @bindable entry: AuditEntry;
   resource: Resource = new Resource();
 
-  constructor(private metadataRepository: MetadataRepository,
-              private modal: Modal) {
+  constructor(private modal: Modal) {
   }
 
   async attached() {
@@ -24,10 +21,7 @@ export class AuditEntryDetailsResource {
       this.resource.resourceClass = resource.resourceClass;
       this.resource.contents = resource.contents;
       this.resource.kind = new ResourceKind();
-      const ids = propertyKeys(this.resource.contents);
-      if (ids.length) {
-        this.resource.kind.metadataList = await this.metadataRepository.getListQuery().filterByIds(ids).get();
-      }
+      this.resource.kind.id = resource.kindId;
     }
   }
 
