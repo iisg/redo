96c815d4d40a3928e6a76464eb9734d8b135d938
Wojciech Frącz <fraczwojciech@gmail.com>
2018-06-05 09:08:09 +0200

    [REPEKA-533] Show resource kind in audit resource details
    Change-Id: I305b766709f3a085869dce5d4c7e795f40b42024

--- src/AdminPanel/src/audit/entry-details/audit-entry-details-resource-contents-modal.ts
+++ src/AdminPanel/src/audit/entry-details/audit-entry-details-resource-contents-modal.ts
@@ -2,16 +2,34 @@ import {Resource} from "../../resources/resource";
 import {AuditEntry} from "../audit-entry";
 import {DialogController} from "aurelia-dialog";
 import {autoinject} from "aurelia-dependency-injection";
+import {MetadataRepository} from "../../resources-config/metadata/metadata-repository";
+import {ResourceKindRepository} from "../../resources-config/resource-kind/resource-kind-repository";
+import {keys, pullAll, toInteger} from "lodash";
 
 @autoinject
 export class AuditEntryDetailsResourceContentsModal {
   resource: Resource;
   entry: AuditEntry;
 
-  constructor(private dialogController: DialogController) {
+  constructor(private dialogController: DialogController,
+              private metadataRepository: MetadataRepository,
+              private resourceKindRepository: ResourceKindRepository) {
   }
 
-  activate(model) {
+  async activate(model) {
+    if (!model.resource.kind.metadataList.length) {
+      const metadataIdsFromContents = keys(model.resource.contents).map(toInteger);
+      try {
+        model.resource.kind = await this.resourceKindRepository.get(model.resource.kind.id, true);
+        const metadataIdsFromRK = model.resource.kind.metadataList.map(m => m.id);
+        pullAll(metadataIdsFromContents, metadataIdsFromRK);
+      } catch (e) {
+      }
+      if (metadataIdsFromContents.length) {
+        const metadataListBasedOnContents = await this.metadataRepository.getListQuery().filterByIds(metadataIdsFromContents).get();
+        model.resource.kind.metadataList = model.resource.kind.metadataList.concat(metadataListBasedOnContents);
+      }
+    }
     this.resource = model.resource;
     this.entry = model.entry;
   }
