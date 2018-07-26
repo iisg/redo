8bffd545506e92b74ebc54dda0ec5e2fc0afa092
Michał Kasprzyk <kasprzyk@protonmail.com>
2018-07-26 09:26:38 +0200

    [REPEKA-542] New workflow edit form look
    Change-Id: Ibb9a174d60763e0c8b1a8dcb5427dc00b08f972e

--- src/AdminPanel/src/common/components/resource-kind/resource-kind-chooser/resource-kind-chooser.ts
+++ src/AdminPanel/src/common/components/resource-kind/resource-kind-chooser/resource-kind-chooser.ts
@@ -1,5 +1,6 @@
 import {autoinject} from "aurelia-dependency-injection";
 import {bindable, ComponentAttached} from "aurelia-templating";
+import {booleanAttribute} from "common/components/boolean-attribute";
 import {ResourceKind} from "resources-config/resource-kind/resource-kind";
 import {ResourceKindRepository} from "resources-config/resource-kind/resource-kind-repository";
 import {oneTime, twoWay} from "../../binding-mode";
@@ -7,11 +8,12 @@ import {oneTime, twoWay} from "../../binding-mode";
 @autoinject
 export class ResourceKindChooser implements ComponentAttached {
   @bindable(twoWay) value: ResourceKind | ResourceKind[];
-  @bindable(oneTime) multiSelect: boolean = false;
-  @bindable disabled: boolean = false;
+  @bindable(oneTime) multiSelect: boolean;
   @bindable resourceClass: string;
+  @bindable disabled: boolean;
   @bindable filter: (resourceKind: ResourceKind) => boolean = () => true;
-  resourceKinds: ResourceKind[] = [];
+  @bindable @booleanAttribute useComputedWidth: boolean;
+  resourceKinds: ResourceKind[];
 
   constructor(private resourceKindRepository: ResourceKindRepository) {
   }
