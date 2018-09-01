da0591ae2b7c224eee96ab7e67114527a07e21c8
skrzypul <blazej.skrzypulec@gmail.com>
2018-09-01 20:36:33 +0200

    [REPEKA-588] Add config for generating reports
    Change-Id: I27df65d991036fda726b4c43a389962d49b6c963

--- src/AdminPanel/tests/e2e/protractor.config.js
+++ src/AdminPanel/tests/e2e/protractor.config.js
@@ -1,6 +1,7 @@
 exports.config = {
-  framework: 'jasmine',
-  specs: ['LoginTests.js', 'MetadataTypeTests.js', 'ResourceTypeTests.js', 'ResourceTests.js', 'TransitionTests.js'],
+  framework: 'jasmine2',
+  specs: ['LoginTests.js'],
+  //specs: ['LoginTests.js', 'MetadataTypeTests.js', 'ResourceTypeTests.js', 'ResourceTests.js', 'TransitionTests.js'],
   getPageTimeout: 120000,
   allScriptsTimeout: 120000,
   capabilities: {
@@ -14,7 +15,37 @@ exports.config = {
       getPageTimeout: 120000;
       allScriptsTimeout: 120000;
       jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
-
+	  
+	  var jasmineReporters = require('jasmine-reporters');
+	  jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: './reports',
+        filePrefix: 'xmloutput'
+    }));
+	
+	var browserName, browserVersion;
+     var capsPromise = browser.getCapabilities();
+ 
+     capsPromise.then(function (caps) {
+        browserName = caps.get('browserName');
+        browserVersion = caps.get('version');
+        platform = caps.get('platform');
+ 
+        var HTMLReport = require('protractor-html-reporter-2');
+ 
+        testConfig = {
+            reportTitle: 'Protractor Test Execution Report',
+            outputPath: './reports',
+            outputFilename: 'ProtractorTestReport',
+            screenshotPath: './screenshots',
+            testBrowser: browserName,
+            browserVersion: browserVersion,
+            modifiedSuiteName: false,
+            screenshotsOnlyOnFailure: true,
+            testPlatform: platform
+        };
+        new HTMLReport().from('./reports/xmloutput.xml', testConfig);
+    });
    },
 }
 
