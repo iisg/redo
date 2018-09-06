a45e78bb6c1b665e68b9a30ca1cefd5ffa468364
Wojciech Frącz <fraczwojciech@gmail.com>
2018-09-06 12:48:41 +0200

    [REPEKA-588] Save E2E report to var/reports
    Change-Id: I96863c2d7ebd7f7e19b5eb84ee1f921ea906f986

--- src/AdminPanel/tests/e2e/protractor.config.js
+++ src/AdminPanel/tests/e2e/protractor.config.js
@@ -7,46 +7,44 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-		args: [ "--headless", '--no-sandbox']
+      args: ["--headless", '--no-sandbox']
     }
   },
-  onPrepare: function(){
-      browser.ignoreSynchronization = true;
-      getPageTimeout: 120000;
-      allScriptsTimeout: 120000;
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
-	  
-	  var jasmineReporters = require('jasmine-reporters');
-	  jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-        consolidateAll: true,
-        savePath: './reports',
-        filePrefix: 'xmloutput'
+  onPrepare: function () {
+    browser.ignoreSynchronization = true;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
+
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: '../../var/reports/e2e',
+      filePrefix: 'xmloutput'
     }));
-	
-	var browserName, browserVersion;
-     var capsPromise = browser.getCapabilities();
- 
-     capsPromise.then(function (caps) {
-        browserName = caps.get('browserName');
-        browserVersion = caps.get('version');
-        platform = caps.get('platform');
- 
-        var HTMLReport = require('protractor-html-reporter-2');
- 
-        testConfig = {
-            reportTitle: 'Protractor Test Execution Report',
-            outputPath: './reports',
-            outputFilename: 'ProtractorTestReport',
-            screenshotPath: './screenshots',
-            testBrowser: browserName,
-            browserVersion: browserVersion,
-            modifiedSuiteName: false,
-            screenshotsOnlyOnFailure: true,
-            testPlatform: platform
-        };
-        new HTMLReport().from('./reports/xmloutput.xml', testConfig);
+
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('protractor-html-reporter-2');
+
+      testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: './reports',
+        outputFilename: 'ProtractorTestReport',
+        screenshotPath: './screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+        testPlatform: platform
+      };
+      new HTMLReport().from('./reports/xmloutput.xml', testConfig);
     });
-   },
-}
+  },
+};
 
 //args: [ "--headless", "--disable-gpu", "--window-size=800x600", '--no-sandbox','-disable-popup-blocking', '-disable-dev-shm-usage' ]
