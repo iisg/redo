ec38beb106178048c7f01c90b609741090d0053b
skrzypul <blazej.skrzypulec@gmail.com>
2018-09-15 22:23:09 +0200

    [REPEKA-642] E2E tests update
    Change-Id: I73a65335ef92289a30bd739fb75ffc262dad4703

--- src/AdminPanel/tests/e2e/protractor.config.js
+++ src/AdminPanel/tests/e2e/protractor.config.js
@@ -10,41 +10,12 @@ exports.config = {
       args: ["--headless", '--no-sandbox']
     }
   },
-  onPrepare: function () {
-    browser.ignoreSynchronization = true;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
-
-    var jasmineReporters = require('jasmine-reporters');
-    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-      consolidateAll: true,
-      savePath: '../../var/reports/e2e',
-      filePrefix: 'xmloutput'
-    }));
-
-    var browserName, browserVersion;
-    var capsPromise = browser.getCapabilities();
-
-    capsPromise.then(function (caps) {
-      browserName = caps.get('browserName');
-      browserVersion = caps.get('version');
-      platform = caps.get('platform');
-
-      var HTMLReport = require('protractor-html-reporter-2');
-
-      testConfig = {
-        reportTitle: 'Protractor Test Execution Report',
-        outputPath: './reports',
-        outputFilename: 'ProtractorTestReport',
-        screenshotPath: './screenshots',
-        testBrowser: browserName,
-        browserVersion: browserVersion,
-        modifiedSuiteName: false,
-        screenshotsOnlyOnFailure: true,
-        testPlatform: platform
-      };
-      new HTMLReport().from('./reports/xmloutput.xml', testConfig);
-    });
-  },
+  onPrepare: function(){
+      browser.ignoreSynchronization = true;
+      getPageTimeout: 120000;
+      allScriptsTimeout: 120000;
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 180000;
+   },
 };
 
 //args: [ "--headless", "--disable-gpu", "--window-size=800x600", '--no-sandbox','-disable-popup-blocking', '-disable-dev-shm-usage' ]
