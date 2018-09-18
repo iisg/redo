09d05ac912c8459d5308cab29b7f1801c2ef4841
skrzypul <blazej.skrzypulec@gmail.com>
2018-09-18 23:40:15 +0200

    [REPEKA-642] Update resource tests
    Change-Id: I6b0461558c5fba0c58e497949206db63c2030c57

--- src/AdminPanel/tests/e2e/ResourceTests.js
+++ src/AdminPanel/tests/e2e/ResourceTests.js
@@ -5,96 +5,93 @@ describe('Resource Tests', function() {
 			return locator.isPresent()
 	   }, 10000);
   }
-  
+
   beforeEach(function() {
 		browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books');
 		browser.driver.manage().window().maximize();
   });
 
   it('Try to add without required metadata value', function() {
-	var EC = protractor.ExpectedConditions;  
-	  
 	waitForElement(element(by.cssContainingText('span', 'Dodaj')));
 	var addResourceButton = element(by.cssContainingText('span', 'Dodaj'));
 	addResourceButton.click();
-	
-	browser.sleep(1000);
+
 	waitForElement(element(by.className('select2-selection__rendered')));
 	var lists = element.all(by.className('select2-selection__rendered'));
-	var listToClick = lists.get(1);	
-	var isClickable = EC.elementToBeClickable(listToClick);
-	browser.wait(isClickable, 10000);
+	var listToClick = lists.get(1);
+	browser.sleep(500);
 	listToClick.click();
+	browser.sleep(500);
 	browser.driver.switchTo().activeElement().sendKeys('Rodzaj_do_testowania_automatycznego');
 	browser.driver.switchTo().activeElement().sendKeys(protractor.Key.ENTER);
 	browser.sleep(500);
-	
+
 	waitForElement(element(by.buttonText('Dodaj')));
 	var addRButton = element(by.buttonText('Dodaj'));
 	addRButton.click();
-	
+
 	waitForElement(element(by.className('help-block validation-message')));
 	var errorMessage = element(by.className('help-block validation-message'));
     expect(errorMessage.getText()).toContain('To pole jest wymagane');
   });
-  
+
   it('Add resource', function() {
 	waitForElement(element(by.cssContainingText('span', 'Dodaj')));
 	var addResourceButton = element(by.cssContainingText('span', 'Dodaj'));
 	addResourceButton.click();
-	
-	browser.sleep(1000);
+
 	waitForElement(element(by.className('select2-selection__rendered')));
 	var lists = element.all(by.className('select2-selection__rendered'));
-	var listToClick = lists.get(1);	
+	var listToClick = lists.get(1);
 	browser.sleep(500);
 	listToClick.click();
 	browser.sleep(500);
 	browser.driver.switchTo().activeElement().sendKeys('Rodzaj_do_testowania_automatycznego');
 	browser.driver.switchTo().activeElement().sendKeys(protractor.Key.ENTER);
 	browser.sleep(500);
-	
-	waitForElement(element(by.className('new-metadata-value-button-container')));	
+
+	waitForElement(element(by.className('new-metadata-value-button-container')));
 	waitForElement(element(by.className('form-control au-target')));
 	var metadataValueFieldBlanks = element(by.className('form-control au-target'));
 	metadataValueFieldBlanks.sendKeys('Value1');
-	
+
 	var newMetadataValuesFields = element.all(by.className('new-metadata-value-button-container'));
 	var secondMetadataValueField = newMetadataValuesFields.get(0);
 	var secondMetadataValueButton = secondMetadataValueField.all(by.className('au-target'));
 	secondMetadataValueButton.get(0).click();
-	
+
 	waitForElement(element(by.className('form-control au-target')));
 	var newMetadataValueFieldBlanks = element.all(by.className('form-control au-target'));
 	newMetadataValueFieldBlanks.get(1).sendKeys('Value2');
-	
+
 	waitForElement(element(by.buttonText('Dodaj')));
 	var addRButton = element(by.buttonText('Dodaj'));
 	addRButton.click();
-	
+
 	waitForElement(element(by.className('au-target resources-list')));
 	var resourcesListNew = element(by.className('au-target resources-list'));
 	expect(resourcesListNew.getText()).toContain('Rodzaj_do_testowania_automatycznego');
   });
-  
-  it('Try to remove required metadata value', function() {	   
+
+  it('Try to remove required metadata value', function() {
 	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
 	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
 	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
 	var lastResource = resourcesList.get(0);
 	lastResource.click();
-	
+
 	waitForElement(element(by.buttonText('Edytuj')));
 	var editButton = element(by.buttonText('Edytuj'));
 	editButton.click();
-	
+
 	waitForElement(element(by.className('buttons')));
 	browser.sleep(1000);
 	var buttonsPanels = element.all(by.className('buttons'));
 	var firstValueButtonPanel = buttonsPanels.get(1);
 	var deleteValueButton = firstValueButtonPanel.all(by.className('au-target')).get(0);
+	browser.sleep(500);
 	deleteValueButton.click();
-	
+
 	waitForElement(element(by.buttonText('Zapisz')));
 	var editButton = element(by.buttonText('Zapisz'));
 	editButton.click();
@@ -103,23 +100,26 @@ describe('Resource Tests', function() {
 	var errorMessage = element(by.className('help-block validation-message'));
     expect(errorMessage.getText()).toContain('Wartość dla tej metadanej jest wymagana');
   });
-  
-  it('Modify metadata value', function() {	   
+
+  it('Modify metadata value', function() {
+	alertDialog = browser.switchTo().alert();
+	alertDialog.accept();
+
 	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
 	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
 	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
 	var lastResource = resourcesList.get(0);
 	lastResource.click();
-	
+
 	waitForElement(element(by.buttonText('Edytuj')));
 	var editButton = element(by.buttonText('Edytuj'));
 	editButton.click();
-		
+
 	waitForElement(element(by.className('form-control au-target')));
 	var metadataValueFields = element.all(by.className('form-control au-target'));
 	metadataValueFields.get(0).clear();
 	metadataValueFields.get(0).sendKeys('NewValue');
-	
+
 	waitForElement(element(by.buttonText('Zapisz')));
 	var editButton = element(by.buttonText('Zapisz'));
 	editButton.click();
@@ -128,26 +128,26 @@ describe('Resource Tests', function() {
 	browser.sleep(1000);
     var metadataValuesList = element(by.className('resource-details'));
 	expect(metadataValuesList.getText()).toContain('NewValue');
-  });  
-  
-  it('Remove optional metadata value', function() {	   
+  });
+
+  it('Remove optional metadata value', function() {
 	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
 	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
 	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
 	var lastResource = resourcesList.get(0);
 	lastResource.click();
-	
+
 	waitForElement(element(by.buttonText('Edytuj')));
 	var editButton = element(by.buttonText('Edytuj'));
 	editButton.click();
-	
+
 	waitForElement(element(by.className('buttons')));
 	browser.sleep(1000);
 	var buttonsPanels = element.all(by.className('buttons'));
 	var firstValueButtonPanel = buttonsPanels.get(2);
 	var deleteValueButton = firstValueButtonPanel.all(by.className('au-target')).get(0);
 	deleteValueButton.click();
-	
+
 	waitForElement(element(by.buttonText('Zapisz')));
 	var editButton = element(by.buttonText('Zapisz'));
 	editButton.click();
@@ -157,7 +157,7 @@ describe('Resource Tests', function() {
     var metadataValuesList = element(by.className('resource-details'));
 	expect(metadataValuesList.getText()).not.toContain('Value2');
   });
-  
+
   it('Delete resource', function() {
 	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
 	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
@@ -165,11 +165,11 @@ describe('Resource Tests', function() {
 	var lastResource = resourcesList.get(0);
 	lastResource.click();
 
-	waitForElement(element(by.buttonText('Usuń')));
-	var deleteButton = element(by.buttonText('Usuń'));
+	waitForElement(element(by.cssContainingText('span', 'Usuń')));
+	var deleteButton = element(by.cssContainingText('span', 'Usuń'));
 	deleteButton.click();
 
-	var confirmButton = element.all(by.className('swal2-confirm btn btn-danger'));
+	var confirmButton = element.all(by.className('swal2-confirm toggle-button red'));
 	confirmButton.click();
 
 	waitForElement(element(by.className('au-target resources-list')));
@@ -178,6 +178,3 @@ describe('Resource Tests', function() {
 	expect(resourcesListNew.getText()).not.toContain('Rodzaj_do_testowania_automatycznego');
   });
 });
-
-
-
