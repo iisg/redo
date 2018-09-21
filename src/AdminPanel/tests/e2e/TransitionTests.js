d26d0b8a852832bb2f0d195571ad5a72ba97ad73
skrzypul <blazej.skrzypulec@gmail.com>
2018-09-21 20:58:37 +0200

    [REPEKA-642] Update transition tests
    Change-Id: Ie9313f24e1e97dfd956874ecf0efe6bb50bd9722

--- src/AdminPanel/tests/e2e/TransitionTests.js
+++ src/AdminPanel/tests/e2e/TransitionTests.js
@@ -5,7 +5,7 @@ describe('Transition Tests', function() {
 			return locator.isPresent()
 	   }, 10000);
   }
-  
+
   beforeEach(function() {
 	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
 	browser.driver.manage().window().maximize();
@@ -14,12 +14,13 @@ describe('Transition Tests', function() {
 	var addResourceButton = element(by.cssContainingText('span', 'Dodaj'));
 	addResourceButton.click();
 
+	waitForElement(element(by.className('current-page-number')));
 	waitForElement(element(by.className('select2-selection__rendered')));
 	var lists = element.all(by.className('select2-selection__rendered'));
 	var listToClick = lists.get(1);
-	browser.sleep(1000);
+	browser.sleep(500);
 	listToClick.click();
-	browser.sleep(1000);
+	browser.sleep(500);
 	browser.driver.switchTo().activeElement().sendKeys('Rodzaj_do_testowania_automatycznego');
 	browser.driver.switchTo().activeElement().sendKeys(protractor.Key.ENTER);
 	browser.sleep(500);
@@ -45,24 +46,6 @@ describe('Transition Tests', function() {
 	waitForElement(element(by.className('au-target resources-list')));
   });
 
-  afterEach(function() {
-	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
-	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
-	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
-	var lastResource = resourcesList.get(0);
-	lastResource.click();
-
-	waitForElement(element(by.buttonText('Usuń')));
-	var deleteButton = element(by.buttonText('Usuń'));
-	deleteButton.click();
-
-	var confirmButton = element.all(by.className('swal2-confirm btn btn-danger'));
-	confirmButton.click();
-
-	waitForElement(element(by.className('au-target resources-list')));
-	browser.sleep(1000);
-  });
-
   it('Try to make a transition without any designated person', function() {
 	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
 	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
@@ -75,6 +58,23 @@ describe('Transition Tests', function() {
 	transitionButton.click();
 
 	expect(element(by.buttonText('Edytuj')).isPresent()).toBe(true);
+
+	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
+	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
+	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
+	var lastResource = resourcesList.get(0);
+	lastResource.click();
+
+	waitForElement(element(by.cssContainingText('span', 'Usuń')));
+	var deleteButton = element(by.cssContainingText('span', 'Usuń'));
+	deleteButton.click();
+
+	var confirmButton = element.all(by.className('swal2-confirm toggle-button red'));
+	confirmButton.click();
+	browser.sleep(500);
+
+	waitForElement(element(by.className('au-target resources-list')));
+	browser.sleep(1000);
   });
 
   it('Try to make a transition without a proper designated person', function() {
@@ -96,10 +96,11 @@ describe('Transition Tests', function() {
 
 	waitForElement(element(by.className('select2-selection__rendered')));
 	var personLists = element.all(by.className('select2-selection__rendered'));
-	browser.sleep(1000);
+	browser.sleep(500);
 	personLists.click();
 	browser.sleep(1000);
-	browser.driver.switchTo().activeElement().sendKeys('Skaniści');
+	browser.driver.switchTo().activeElement().sendKeys('ZZZ_automatyczne_testowanie');
+	browser.sleep(1500);
 	browser.driver.switchTo().activeElement().sendKeys(protractor.Key.ENTER);
 	browser.sleep(500);
 
@@ -111,7 +112,25 @@ describe('Transition Tests', function() {
 	var transitionButton = element(by.cssContainingText('span', 'przejście'));
 	transitionButton.click();
 
+	browser.sleep(3000);
 	expect(element(by.buttonText('Edytuj')).isPresent()).toBe(true);
+
+	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
+	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
+	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
+	var lastResource = resourcesList.get(0);
+	lastResource.click();
+
+	waitForElement(element(by.cssContainingText('span', 'Usuń')));
+	var deleteButton = element(by.cssContainingText('span', 'Usuń'));
+	deleteButton.click();
+
+	var confirmButton = element.all(by.className('swal2-confirm toggle-button red'));
+	confirmButton.click();
+	browser.sleep(500);
+
+	waitForElement(element(by.className('au-target resources-list')));
+	browser.sleep(1000);
   });
 
   it('Try to make a transition without required metadata', function() {
@@ -130,13 +149,15 @@ describe('Transition Tests', function() {
 	var personMetadataValueField = newMetadataValuesFields.get(0);
 	var personMetadataValueButton = personMetadataValueField.all(by.className('au-target'));
 	personMetadataValueButton.get(0).click();
+	browser.sleep(500);
 
 	waitForElement(element(by.className('select2-selection__rendered')));
 	var personLists = element.all(by.className('select2-selection__rendered'));
-	browser.sleep(1000);
+	browser.sleep(500);
 	personLists.click();
 	browser.sleep(1000);
-	browser.driver.switchTo().activeElement().sendKeys('admin');
+	browser.driver.switchTo().activeElement().sendKeys('AAA_testowanie_automatyczne');
+	browser.sleep(1500);
 	browser.driver.switchTo().activeElement().sendKeys(protractor.Key.ENTER);
 	browser.sleep(500);
 
@@ -162,6 +183,25 @@ describe('Transition Tests', function() {
 	waitForElement(element(by.className('help-block validation-message')));
 	var errorMessage = element(by.className('help-block validation-message'));
     expect(errorMessage.getText()).toContain('Wartość dla tej metadanej jest wymagana');
+
+	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
+	alertDialog = browser.switchTo().alert();
+	alertDialog.accept();
+	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
+	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
+	var lastResource = resourcesList.get(0);
+	lastResource.click();
+
+	waitForElement(element(by.cssContainingText('span', 'Usuń')));
+	var deleteButton = element(by.cssContainingText('span', 'Usuń'));
+	deleteButton.click();
+
+	var confirmButton = element.all(by.className('swal2-confirm toggle-button red'));
+	confirmButton.click();
+	browser.sleep(500);
+
+	waitForElement(element(by.className('au-target resources-list')));
+	browser.sleep(1000);
   });
 
   it('Automatic ascription of a designated person', function() {
@@ -183,10 +223,11 @@ describe('Transition Tests', function() {
 
 	waitForElement(element(by.className('select2-selection__rendered')));
 	var personLists = element.all(by.className('select2-selection__rendered'));
-	browser.sleep(1000);
+	browser.sleep(500);
 	personLists.click();
 	browser.sleep(1000);
-	browser.driver.switchTo().activeElement().sendKeys('admin');
+	browser.driver.switchTo().activeElement().sendKeys('AAA_testowanie_automatyczne');
+	browser.sleep(1500);
 	browser.driver.switchTo().activeElement().sendKeys(protractor.Key.ENTER);
 	browser.sleep(500);
 
@@ -206,6 +247,23 @@ describe('Transition Tests', function() {
 	waitForElement(element(by.buttonText('Edytuj')));
     var metadataValuesList = element(by.className('resource-details'));
 	expect(metadataValuesList.getText()).toContain('admin');
+
+	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
+	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
+	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
+	var lastResource = resourcesList.get(0);
+	lastResource.click();
+
+	waitForElement(element(by.cssContainingText('span', 'Usuń')));
+	var deleteButton = element(by.cssContainingText('span', 'Usuń'));
+	deleteButton.click();
+
+	var confirmButton = element.all(by.className('swal2-confirm toggle-button red'));
+	confirmButton.click();
+	browser.sleep(500);
+
+	waitForElement(element(by.className('au-target resources-list')));
+	browser.sleep(1000);
   });
 
   it('Sucessful transition', function() {
@@ -227,10 +285,11 @@ describe('Transition Tests', function() {
 
 	waitForElement(element(by.className('select2-selection__rendered')));
 	var personLists = element.all(by.className('select2-selection__rendered'));
-	browser.sleep(1000);
+	browser.sleep(500);
 	personLists.click();
 	browser.sleep(1000);
-	browser.driver.switchTo().activeElement().sendKeys('admin');
+	browser.driver.switchTo().activeElement().sendKeys('AAA_testowanie_automatyczne');
+	browser.sleep(1500);
 	browser.driver.switchTo().activeElement().sendKeys(protractor.Key.ENTER);
 	browser.sleep(500);
 
@@ -250,8 +309,22 @@ describe('Transition Tests', function() {
 	waitForElement(element(by.buttonText('Edytuj')));
     var metadataValuesList = element(by.className('resource-details'));
 	expect(metadataValuesList.getText()).toContain('KONIEC');
+
+	browser.get('https://repekadev.fslab.agh.edu.pl/admin/resources/books?currentPageNumber=1&resourcesPerPage=1000');
+	waitForElement(element(by.className('au-animate fade-inup-outdown au-target')));
+	var resourcesList = element.all(by.className('au-animate fade-inup-outdown au-target'));
+	var lastResource = resourcesList.get(0);
+	lastResource.click();
+
+	waitForElement(element(by.cssContainingText('span', 'Usuń')));
+	var deleteButton = element(by.cssContainingText('span', 'Usuń'));
+	deleteButton.click();
+
+	var confirmButton = element.all(by.className('swal2-confirm toggle-button red'));
+	confirmButton.click();
+	browser.sleep(500);
+
+	waitForElement(element(by.className('au-target resources-list')));
+	browser.sleep(1000);
   });
 });
-
-
-
