f26fc6695eb6b1e4427ced80fb97d70abf7e4f48
Kevin <kevinpietrzyk@gmail.com>
2018-11-02 17:25:25 +0100

    [REPEKA-679] Restore to original value for display strategy metadata's
    Change-Id: I19cc4d87c536dd65a3056a60da5bc8bd6f750148

--- src/AdminPanel/src/common/components/wysiwyg-editor/wysiwyg-editor.ts
+++ src/AdminPanel/src/common/components/wysiwyg-editor/wysiwyg-editor.ts
@@ -2,38 +2,38 @@ import {bindable} from "aurelia-templating";
 import {twoWay} from "common/components/binding-mode";
 
 export class WysiwygEditor {
-    @bindable(twoWay) value;
-    @bindable placeholder: string;
-    @bindable disabled: boolean;
-    editor: HTMLInputElement;
-    private joditEditor: any;
+  @bindable(twoWay) value;
+  @bindable placeholder: string;
+  @bindable disabled: boolean;
+  editor: HTMLInputElement;
+  private joditEditor: any;
 
-    contentChanged(newValue: string) {
-        if (this.joditEditor && this.joditEditor.value != newValue) {
-            this.joditEditor.value = newValue;
-        }
+  valueChanged(newValue: string) {
+    if (this.joditEditor) {
+      this.joditEditor.value = newValue;
     }
+  }
 
-    attached() {
-        const Jodit = require('jodit'); // tslint:disable-line
-        const configuration = {
-            "buttonsXS": "source, |, bold, image, |, brush, paragraph, , align, |, undo, redo, |, dots",
-            "toolbarSticky": false,
-            "width": "100%",
-            "placeholder": this.placeholder,
-            "uploader": {
-                insertImageAsBase64URI: true
-            }
-        };
-        if (this.disabled) {
-            configuration["toolbarAdaptive"] = false;
-            configuration["buttons"] = "source, |, fullsize, selectall, print, about";
-            configuration["readonly"] = true;
-        }
-        this.joditEditor = new Jodit(this.editor, configuration);
-        this.joditEditor.value = this.value;
-        this.joditEditor.events.on('change', () => {
-            this.value = this.joditEditor.value;
-        });
+  attached() {
+    const Jodit = require('jodit'); // tslint:disable-line
+    const configuration = {
+      "buttonsXS": "source, |, bold, image, |, brush, paragraph, , align, |, undo, redo, |, dots",
+      "toolbarSticky": false,
+      "width": "100%",
+      "placeholder": this.placeholder,
+      "uploader": {
+        insertImageAsBase64URI: true
+      }
+    };
+    if (this.disabled) {
+      configuration["toolbarAdaptive"] = false;
+      configuration["buttons"] = "source, |, fullsize, selectall, print, about";
+      configuration["readonly"] = true;
     }
+    this.joditEditor = new Jodit(this.editor, configuration);
+    this.joditEditor.value = this.value;
+    this.joditEditor.events.on('change', () => {
+      this.value = this.joditEditor.value;
+    });
+  }
 }
