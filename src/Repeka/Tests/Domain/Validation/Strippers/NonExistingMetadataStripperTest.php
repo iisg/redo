c7a172c0c51034377dacb9a5175f0b6a6ed5ff18
karolkozak <karol2868@gmail.com>
2018-11-06 22:39:28 +0100

    [REPEKA-708] Strip non existing metadata from workflow places
    Change-Id: I29167cb2d739dc1e821f828b007fd139c66e1ea6

---
+++ src/Repeka/Tests/Domain/Validation/Strippers/NonExistingMetadataStripperTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Repeka\Tests\Domain\Validation\Strippers;
+
+use Repeka\Domain\Entity\Workflow\ResourceWorkflowPlace;
+use Repeka\Domain\Validation\Strippers\NonExistingMetadataStripper;
+use Repeka\Tests\Traits\StubsTrait;
+
+class NonExistingMetadataStripperTest extends \PHPUnit_Framework_TestCase {
+    use StubsTrait;
+    /** @var  NonExistingMetadataStripper */
+    private $nonExistingMetadataStripper;
+
+    protected function setUp() {
+        $metadata = $this->createMetadataMock();
+        $metadataRepository = $this->createMetadataRepositoryStub([$metadata]);
+        $metadataRepository->method('findByQuery')->willReturn([$metadata]);
+        $this->nonExistingMetadataStripper = new NonExistingMetadataStripper($metadataRepository);
+    }
+
+    public function testFiltersNonExistingMetadata() {
+        $places = [
+            new ResourceWorkflowPlace(['PL' => 'label1'], 'id1', [1, 2]),
+            new ResourceWorkflowPlace(['PL' => 'label2'], 'id2', [1], [2]),
+        ];
+        $result = $this->nonExistingMetadataStripper->removeNonExistingMetadata($places, 'books');
+        $expectedPlaces = [
+            new ResourceWorkflowPlace(['PL' => 'label1'], 'id1', [1]),
+            new ResourceWorkflowPlace(['PL' => 'label2'], 'id2', [1]),
+        ];
+        $this->assertEquals($expectedPlaces, $result);
+    }
+}
