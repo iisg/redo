2bbbaccb2d83486134e79535068bbfb452470773
Pawel Grochola <grocholapawel@gmail.com>
2019-01-20 20:03:31 +0100

    [REPEKA-811] Search by label metadata in admin panel
    Change-Id: Ib83bf382186fd9324f1ed265fe35bb42c8f9cfc2

--- src/AdminPanel/src/resources-config/metadata/metadata-chooser.ts
+++ src/AdminPanel/src/resources-config/metadata/metadata-chooser.ts
@@ -3,12 +3,14 @@ import {I18N} from "aurelia-i18n";
 import {bindable, customElement} from "aurelia-templating";
 import {EntityChooser} from "common/components/entity-chooser/entity-chooser";
 import {MetadataRepository} from "./metadata-repository";
+import {Metadata} from "./metadata";
 
 @customElement('metadata-chooser')
 @autoinject
 export class MetadataChooser extends EntityChooser {
   @bindable resourceClass: string | string[];
   @bindable control: string | string[];
+  @bindable additionalMetadataIds: number[] = [];
   private loadingMetadataList = false;
   private reloadMetadataList = false;
 
@@ -38,11 +40,18 @@ export class MetadataChooser extends EntityChooser {
       this.reloadMetadataList = true;
     } else {
       this.loadingMetadataList = true;
-      this.metadataRepository.getListQuery()
+      const controlMetadata = this.metadataRepository.getListQuery()
         .filterByResourceClasses(this.resourceClass)
         .filterByControls(this.control)
         .onlyTopLevel()
+        .get();
+      const additionalMetadata = this.additionalMetadataIds.length > 0
+        ? this.metadataRepository.getListQuery()
+        .filterByIds(this.additionalMetadataIds)
         .get()
+        : [];
+      Promise.all([additionalMetadata, controlMetadata])
+        .then(metadataListList => ([] as Metadata[]).concat(...metadataListList))
         .then(metadataList => {
           this.loadingMetadataList = false;
           if (this.reloadMetadataList) {
