db6ceecabf3d70e7df1fc373b0a24f851751e68e
Wojciech Frącz <fraczwojciech@gmail.com>
2019-01-29 16:16:49 +0100

    [REPEKA-818] New controller for FTS instead of abusing exposed endpoints
    Change-Id: Iba86b36f6836367063302e2cd829992ca013f1f6

--- src/Repeka/Application/Service/ResourcesEndpointsExposer.php
+++ src/Repeka/Application/Service/ResourcesEndpointsExposer.php
@@ -15,16 +15,7 @@ class ResourcesEndpointsExposer {
     public function loadRoutes(): RouteCollection {
         $builder = new RouteCollectionBuilder();
         foreach ($this->exposedEndpoints as $path => $endpoint) {
-            if (isset($endpoint['fts'])) {
-                $builder->addRoute(
-                    new FtsResourcesExposedEndpointRoute(
-                        $path,
-                        $endpoint['template'] ?? '',
-                        $endpoint['fts'],
-                        $endpoint['headers']
-                    )
-                );
-            } elseif (isset($endpoint['deposit'])) {
+            if (isset($endpoint['deposit'])) {
                 $builder->addRoute(
                     new ResourceDepositExposedEndpointRoute($path, $endpoint['template'] ?? '', $endpoint['deposit'], $endpoint['headers'])
                 );
