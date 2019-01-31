4bc2556e8454c619115638d1f93031b3f820d99f
Wojciech Frącz <fraczwojciech@gmail.com>
2019-01-31 16:21:56 +0100

    [REPEKA-818] Fix "see on website link", change exit to application name
    Change-Id: I3d82d61c78fb0eb86c8dbe921631ef6404e8ad62

--- src/AdminPanel/src/common/components/top-bar/top-bar.ts
+++ src/AdminPanel/src/common/components/top-bar/top-bar.ts
@@ -5,8 +5,10 @@ import {FrontendConfig} from "../../../config/FrontendConfig";
 @autoinject
 export class TopBar {
   userIp: string;
+  applicationName: string;
 
   constructor(private router: Router) {
     this.userIp = FrontendConfig.get('userIp');
+    this.applicationName = FrontendConfig.get('application_name');
   }
 }
