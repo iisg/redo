166f7cec4b40cffeabb66133b55a346535761a4e
Pawel Grochola <grocholapawel@gmail.com>
2019-02-19 13:44:19 +0100

    [REPEKA-870] Update config; print changed names in migration
    Change-Id: I3a1f06a4778aa8706f1e880aa76b292554a97ad1

--- app/DoctrineMigrations/Version20190216191737.php
+++ app/DoctrineMigrations/Version20190216191737.php
@@ -15,10 +15,14 @@ class Version20190216191737 extends RepekaMigration {
             'SELECT id, name, resource_class FROM metadata AS m1
              WHERE id > 0 AND EXISTS(SELECT * FROM metadata AS m2 WHERE m1.id <> m2.id AND m1.name = m2.name)'
         );
+        if (!empty($duplicatedMetadata)) {
+            $this->write('Renamed metadata (old => new):');
+        }
         foreach ($duplicatedMetadata as $metadata) {
             $newName = $metadata['resource_class'] . '_' . $metadata['name'];
             $params = ['name' => $newName, 'id' => $metadata['id']];
             $this->addSql('UPDATE metadata SET name = :name WHERE id = :id', $params);
+            $this->write($metadata['name'] . ' => ' . $newName);
         }
     }
 }
