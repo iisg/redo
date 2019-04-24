569f064c8ddf72db16fd492e2a7d14619a659906
kozak <karol2868@gmail.com>
2019-04-24 18:41:28 +0200

    [REPEKA-1013] Fix access for unauthenticated user for open resource
    Change-Id: I5378271ada82ad32929983edafe4274c94037ea1

--- src/Repeka/Application/Controller/Site/ResourcesExposureController.php
+++ src/Repeka/Application/Controller/Site/ResourcesExposureController.php
@@ -56,7 +56,7 @@ class ResourcesExposureController extends Controller {
         $responseData = ['r' => $resource, 'resource' => $resource];
         $this->trackUsage($endpointUsageTrackingKey, $request, $resource);
         $response = $this->render($template, $responseData);
-        $this->denyAccessUnlessGranted(['VIEW'], $resource);
+        $this->denyAccessUnlessGranted(['METADATA_VISIBILITY'], $resource);
         if ($response->getContent()) {
             $response->headers->add($headers);
             return $response;
