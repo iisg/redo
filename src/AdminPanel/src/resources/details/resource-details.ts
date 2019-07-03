2fd24d9a23ce656f0538739ccc085e54d7f0dd8b
Wojciech Frącz <fraczwojciech@gmail.com>
2019-07-03 22:47:14 +0200

    [REPEKA-1068] Fix always possible cloning of top level resources
    Change-Id: I4272ce72b58a943563145001141f1c819c721965

--- src/AdminPanel/src/resources/details/resource-details.ts
+++ src/AdminPanel/src/resources/details/resource-details.ts
@@ -227,9 +227,9 @@ export class ResourceDetails implements RoutableComponentActivate {
 
   @computedFrom('parentResource', 'resource')
   get isCloneAllowed(): boolean {
-    return this.resource.kind.allowedToClone
-      && (this.parentResource && this.isReproductor.toView(this.parentResource))
-      || (!this.parentResource && this.hasRole.toView('ADMIN', this.resource.resourceClass));
+    return this.resource.kind.allowedToClone &&
+      ((this.parentResource && this.isReproductor.toView(this.parentResource))
+        || (!this.parentResource && this.hasRole.toView('ADMIN', this.resource.resourceClass)));
   }
 
   cloneResource(cloneTimes: number): Promise<any> {
